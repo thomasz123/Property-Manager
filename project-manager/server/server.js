68242585f823d8995e829fa77e4fd917ce63292e
Thomas <tzhang12062004@gmail.com>
2025-08-20 01:28:06 -0400

    started proj

---
+++ project-manager/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from Express!" })
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
